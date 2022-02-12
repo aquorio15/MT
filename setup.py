3e0e98ce5a78b3d8f856422788e3326eec96bd8a
aquorio15 <91369740+aquorio15@users.noreply.github.com>
2022-02-12 20:44:40 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='A python implementation of OpenNMT',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='1.2.0',
+    version='2.2.0',
     packages=find_packages(),
     project_urls={
         "Documentation": "http://opennmt.net/OpenNMT-py/",
@@ -19,17 +19,15 @@ setup(
         "Gitter": "https://gitter.im/OpenNMT/OpenNMT-py",
         "Source": "https://github.com/OpenNMT/OpenNMT-py/"
     },
+    python_requires=">=3.5",
     install_requires=[
-        "six",
-        "tqdm",
         "torch>=1.6.0",
-        "torchtext==0.4.0",
-        "future",
+        "torchtext==0.5.0",
         "configargparse",
-        "tensorboard>=1.14",
+        "tensorboard>=2.3",
         "flask",
         "waitress",
-        "pyonmttok==1.*;platform_system=='Linux'",
+        "pyonmttok>=1.23,<2;platform_system=='Linux' or platform_system=='Darwin'",
         "pyyaml",
     ],
     entry_points={
@@ -37,9 +35,9 @@ setup(
             "onmt_server=onmt.bin.server:main",
             "onmt_train=onmt.bin.train:main",
             "onmt_translate=onmt.bin.translate:main",
-            "onmt_preprocess=onmt.bin.preprocess:main",
             "onmt_release_model=onmt.bin.release_model:main",
-            "onmt_average_models=onmt.bin.average_models:main"
+            "onmt_average_models=onmt.bin.average_models:main",
+            "onmt_build_vocab=onmt.bin.build_vocab:main"
         ],
     }
 )
